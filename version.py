c607f0bb9d6cd6983f9297093be0e2e2736207ce
Wu-Chen Su <wu-chensu@Wu-Chens-MacBook-Pro.local>
2021-08-06 09:33:15 +0800

    update version

--- version.py
+++ version.py
@@ -1,5 +1,3 @@
 def get_release(self):
-    # hard-coded release for demo purpose
-    # "self" is a dumper instance, see:
-    # https://github.com/biothings/biothings.api/blob/master/biothings/hub/dataload/dumper.py
+    # 04/2021
     return "2.0"
