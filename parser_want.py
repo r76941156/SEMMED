4fe439724dbda206bf21e3f813b29edd41914eab
Wu-Chen Su <wu-chensu@Wu-Chens-MacBook-Pro.local>
2021-08-21 17:51:33 +0800

    update parser

---
+++ parser_want.py
@@ -0,0 +1,103 @@
+from collections import defaultdict
+import csv
+import os
+import sys
+import pandas as pd
+import yaml
+import json
+
+
+names = pd.read_csv("https://metamap.nlm.nih.gov/Docs/SemanticTypes_2018AB.txt", sep="|",
+                   names=['abv', 'ID', 'label'])
+type_label = dict(zip(names.abv, names.label))
+
+
+def load_data(data_folder):
+    def construct_rec(line):
+      if (line[1]!='35392205'):
+        sub_umls=line[4]
+        obj_umls=line[8]
+        predication_id=line[0]
+        print(predication_id)
+        sub_semtype=line[6]
+        sub_name=line[5]
+        obj_semtype=line[10]
+        obj_name=line[9]
+        pmid=int(line[2])
+        pred=line[3]
+        sub_novelty=line[7]
+        obj_novelty=line[11]
+        
+        sub_semtype_name=None
+        obj_semtype_name=None
+        if (sub_semtype in type_label):
+         sub_semtype_name=type_label[sub_semtype]
+       
+        if (obj_semtype in type_label):
+         obj_semtype_name=type_label[obj_semtype]
+       
+        if ("C" not in sub_umls): ###one or more gene ids
+           sub_umls=sub_umls.split("|") 
+           sub_name=sub_name.split("|")
+        else:
+           if ('|' in sub_umls):
+            sub_umls=[sub_umls.split("|")[0]]
+            sub_name=[sub_name.split("|")[0]]
+           else:
+            sub_umls=[sub_umls]
+            sub_name=[sub_name]
+
+        if ("C" not in obj_umls): ###one or more gene ids
+           obj_umls=obj_umls.split("|")
+           obj_name=obj_name.split("|") 
+        else:
+           if ('|' in obj_umls):
+              obj_umls=[obj_umls.split("|")[0]]
+              obj_name=[obj_name.split("|")[0]]
+           else:
+              obj_umls=[obj_umls]
+              obj_name=[obj_name]
+
+        for sub_idx,sub_id in enumerate(sub_umls):
+         for obj_idx,obj_id in enumerate(obj_umls):
+           rec_related[predication_id]={
+  
+            "_id": "PREDICATION_ID:"+predication_id,
+            "predicate": pred,
+            "pmid": pmid,
+            "subject":{
+                    "umls": sub_id,
+                    "name": sub_name[sub_idx],
+                    "semantic_type_abbreviation": sub_semtype,
+                    "semantic_type_name": sub_semtype_name,
+                    "novelty": sub_novelty
+                  },
+            "object":{
+                   "id": obj_id,
+                   "name": obj_name[obj_idx],
+                   "semantic_type_abbreviation": obj_semtype,
+                   "semantic_type_name": obj_semtype_name, 
+                   "novelty": obj_novelty
+                  }
+           }
+        yield rec_related[predication_id]
+        del rec_related[predication_id]
+
+    edges_path = os.path.join(data_folder, "semmed_0821.csv")
+   
+
+    with open(edges_path) as f:
+       csv_total=sum(1 for line in f)
+
+    rec_related = {}
+    with open(edges_path) as f:
+        csv_reader = csv.reader(f, delimiter=';')
+        next(csv_reader)
+        count=0
+        for _item in csv_reader:
+             count+=1
+             if (count<=10000):
+                print("Data Generation Progess:",str(count)+"/"+str(csv_total))
+                construct_rec(_item)
+                print("=====")
+        print("Data Generation is Done.")   
