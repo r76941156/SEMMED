3c94ad21c8195233d3b0067b6249c2efb3e90b3b
Wu-Chen Su <wu-chensu@Wu-Chens-MacBook-Pro.local>
2021-08-24 17:22:15 +0800

    update parser

--- parser.py
+++ parser.py
@@ -7,14 +7,13 @@ import yaml
 import json
 
 
-
 def load_data(data_folder):
-    rec_related={}
     def construct_rec(line):
       if (line[1]!='35392205'):
         sub_umls=line[4]
         obj_umls=line[8]
         predication_id=line[0]
+        print(predication_id)
         sub_semtype=line[6]
         sub_name=line[5]
         obj_semtype=line[10]
@@ -25,7 +24,7 @@ def load_data(data_folder):
         obj_novelty=line[11]
         sub_id_field="umls"
         obj_id_field="umls"
-        
+ 
         sub_semtype_name=None
         obj_semtype_name=None
         if (sub_semtype in type_label):
@@ -37,7 +36,7 @@ def load_data(data_folder):
         if ("C" not in sub_umls): ###one or more gene ids
            sub_umls=sub_umls.split("|") 
            sub_name=sub_name.split("|")
-           sub_id_field="ncbigene" 
+           sub_id_field="ncbigene"         
         else:
            if ('|' in sub_umls):
             sub_umls=[sub_umls.split("|")[0]]
@@ -48,8 +47,8 @@ def load_data(data_folder):
 
         if ("C" not in obj_umls): ###one or more gene ids
            obj_umls=obj_umls.split("|")
-           obj_name=obj_name.split("|")
-           obj_id_field="ncbigene"  
+           obj_name=obj_name.split("|") 
+           obj_id_field="ncbigene"
         else:
            if ('|' in obj_umls):
               obj_umls=[obj_umls.split("|")[0]]
@@ -58,21 +57,30 @@ def load_data(data_folder):
               obj_umls=[obj_umls]
               obj_name=[obj_name]
 
+        id_count=0
         for sub_idx,sub_id in enumerate(sub_umls):
          for obj_idx,obj_id in enumerate(obj_umls):
-           rec_related[predication_id]={
-  
-            "_id": "PREDICATION_ID:"+predication_id,
-            "predicate": pred,
-            "pmid": pmid,
-            "subject":{
+           id_count+=1
+
+           if (len(sub_umls)==1): 
+              id_value=predication_id 
+           else: 
+              id_value=predication_id+"_"+str(id_count)
+
+
+           rec_dict={
+             "_id": id_value,
+             "predicate": pred,
+             "predication_id":predication_id,
+             "pmid": pmid,
+             "subject":{
                     sub_id_field: sub_id,
                     "name": sub_name[sub_idx],
                     "semantic_type_abbreviation": sub_semtype,
                     "semantic_type_name": sub_semtype_name,
                     "novelty": sub_novelty
                   },
-            "object":{
+             "object":{
                    obj_id_field: obj_id,
                    "name": obj_name[obj_idx],
                    "semantic_type_abbreviation": obj_semtype,
@@ -80,34 +88,33 @@ def load_data(data_folder):
                    "novelty": obj_novelty
                   }
            }
-           if (not sub_semtype_name): del rec_related[predication_id]["subject"]["semantic_type_name"]
-           if (not obj_semtype_name): del rec_related[predication_id]["object"]["semantic_type_name"]     
-           yield rec_related.values
+           if (not sub_semtype_name): del rec_dict["subject"]["semantic_type_name"]
+           if (not obj_semtype_name): del rec_dict["object"]["semantic_type_name"]
+           yield rec_dict
 
-    edges_path = os.path.join(data_folder, "semmed_small.csv")
-    mapping_path = os.path.join(data_folder, "SemanticTypes_2018AB.txt")
+           #if id_value not in rec_related:
+           # rec_related[id_value]=rec_dict
+  
 
-    with open(edges_path) as f:
-       next(f) 
-       csv_total=sum(1 for line in f)
-
+    edges_path = os.path.join(data_folder, "semmed_0821.csv")
+    mapping_path = os.path.join(data_folder, "SemanticTypes_2013AA.txt")
     names = pd.read_csv(mapping_path, sep="|",names=['abv', 'ID', 'label'])
     type_label = dict(zip(names.abv, names.label))
 
-    #rec_related = {}
-    count=0
+    with open(edges_path) as f:
+       next(f)
+       csv_total=sum(1 for line in f)
+
+    rec_related = {}
     with open(edges_path) as f:
         csv_reader = csv.reader(f, delimiter=';')
         next(csv_reader)
+        count=0
         for _item in csv_reader:
              count+=1
-             print("Data Preparation Progess:",str(count)+"/"+str(csv_total))
-             construct_rec(_item)
-             print("=====")
-        print("Data Generation is Done.")   
-        count=0
-        
-    #for rec in rec_related.values():
-    #    count+=1
-    #    print("Data Generation Progess:",str(count)+"/"+str(csv_total))
-    #    yield rec   
+             if (count>0):
+                print("Data Prep Progess:",str(count)+"/"+str(csv_total))
+                construct_rec(_item)
+                print("=====")
+        print("Data Prep is Done.")   
+
